a7416f8e5970a5579529a9401e8f49bba30b489f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-04-01 01:51:22 +0500

    BMI Calculator

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# BMI Calculator 
+
+## BMI is an inexpensive and easy screening method for weight category—underweight,healthy weight, overweight, and obesity.
---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log('---------Welcome To BMI Calculator---------');
+console.log("");
+console.log(`"BMI is an inexpensive and easy screening method for weight category—underweight,
+healthy weight, overweight, and obesity."`);
+console.log("");
+console.log("***Let Start***");
+let answer = await inquirer.prompt([
+    {
+        message: "Enter your body weight in Kilogram(Kg)",
+        type: "number",
+        name: "weight"
+    },
+    {
+        message: "Enter your height in centimeter(cm)",
+        type: "number",
+        name: "height"
+    }
+]);
+if (answer.weight && answer.height) {
+    let heightInMeter = answer.height / 100;
+    let bmi = answer.weight / (heightInMeter * heightInMeter);
+    if (bmi < 18.5) {
+        console.log(`BMI = ${bmi} (UnderWeight)`);
+    }
+    else if (bmi >= 18.5 && bmi <= 25) {
+        console.log(`BMI = ${bmi} (Normal)`);
+    }
+    else if (bmi > 25 && bmi <= 30) {
+        console.log(`BMI = ${bmi} (OverWeight)`);
+    }
+    else {
+        console.log(`BMI = ${bmi} (Obesity)`);
+    }
+}
+else {
+    console.log("Enter valid numbers");
+}
